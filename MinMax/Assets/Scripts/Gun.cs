0ee37ecb8b6c366a2f149bc3b46227a250bf8287
calcruikshank <calcruikshank@gmail.com>
2022-07-16 10:48:04 -0600

    slapms

--- MinMax/Assets/Scripts/Gun.cs
+++ MinMax/Assets/Scripts/Gun.cs
@@ -11,7 +11,10 @@ public class Gun : MonoBehaviour
     //Timing stuff
     private float nextFire = 0;
     private float curTime = 0;
-    
+
+
+    [SerializeField] Transform wand;
+    float DistanceBetweenWandAndGun;
     public void Init(PlayerController playerSent)
     {
         playerOwningGun = playerSent;
@@ -20,20 +23,74 @@ public class Gun : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         curTime += Time.deltaTime;
-        if(curTime >= playerOwningGun.stats.AttackCooldown){
+        if (curTime >= playerOwningGun.stats.AttackCooldown)
+        {
             curTime = 0;
-            if(fire){
-                Bullet newBullet = Instantiate(bullet, transform.position, transform.rotation).GetComponent<Bullet>();
-                newBullet.Init(playerOwningGun,transform.position + transform.forward * playerOwningGun.stats.ProjectileRange);
+            if (fire)
+            {
+                switch (playerOwningGun.stats.NumberOfProjectiles)
+                {
+                    case 1:
+                        boom(0);
+                        break;
+                    case 2:
+                        boom(15);
+                        boom(-15);
+                        break;
+                    case 6:
+                        boom(160);
+                        boom(180);
+                        boom(200);
+                        boom(20);
+                        boom(0);
+                        boom(-25);
+                        break;
+                    case 3:
+                        boom(20);
+                        boom(0);
+                        boom(-25);
+                        break;
+                    case 4:
+                        boom(15);
+                        boom(25);
+                        boom(-15);
+                        boom(-25);
+                        break;
+                    case 5:
+                        boom(0);
+                        boom(90);
+                        boom(45);
+                        boom(-45);
+                        boom(-90);
+                        break;
+                    case 7:
+                        boom(0);
+                        boom(90);
+                        boom(45);
+                        boom(-45);
+                        boom(-90);
+                        boom(160);
+                        boom(200);
+                        break;
+                }
+
             }
 
         }
+    }
+
+    void boom(float angleOffset)
+    {
+        Bullet b;
+        b = Instantiate(bullet, transform.position, transform.rotation).GetComponent<Bullet>();
+        b.transform.localEulerAngles = new Vector3(b.transform.localEulerAngles.x, b.transform.localEulerAngles.y + angleOffset, b.transform.localEulerAngles.z);
+        b.Init(playerOwningGun, transform.position + b.transform.forward * playerOwningGun.stats.ProjectileRange);
     }
 }
