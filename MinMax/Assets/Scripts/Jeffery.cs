a3c5ed21f0b21a86c2ac6618acc06222db42e2e1
Travis <travis@lastgameboard.com>
2022-07-16 14:15:35 -0600

    moon walking jeff

--- MinMax/Assets/Scripts/Jeffery.cs
+++ MinMax/Assets/Scripts/Jeffery.cs
@@ -8,6 +8,10 @@ public class Jeffery : MonoBehaviour
     PlayerController controller;
     PlayerController target = null;
 
+    float keepDistance = 10f;
+    float keepRange = 2f;
+    float keepFuzz = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +26,21 @@ public class Jeffery : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SelectTarget();
-        
+        InvokeRepeating("SelectTarget", 2.0f, 0.3f);
+        InvokeRepeating("Fuzz", 2.0f, 2f);
+        LookAt();
         switch(state){
             case State.Chasing:
                 if(target != null){
+                    var distance = Vector3.Distance(target.transform.position, transform.position);
                     var dir = target.transform.position - transform.position;
-                    controller.inputMovement = new Vector2(dir.x * .1f,dir.z *.1f);            
+                    if( distance > keepDistance + (keepRange + keepFuzz)){
+                        controller.inputMovement = new Vector2(dir.x * .1f,dir.z *.1f);     
+                    }else if(distance < keepDistance - (keepRange + keepFuzz)){
+                        controller.inputMovement = new Vector2(dir.x * .1f * -1,dir.z *.1f * -1);     
+                    }else{
+                        controller.inputMovement = new Vector2(0,0);
+                    }
                 }
             break;
         }
@@ -43,9 +55,16 @@ public class Jeffery : MonoBehaviour
         }
     }
 
+    void Fuzz(){
+        //keepFuzz = Random.Range(-1,1);
+    }
+
     void LookAt(){
-                            var dir = target.transform.position - transform.position;
-                    controller.inputMovement = new Vector2(dir.x * .1f,dir.z *.1f);       
+        if(target != null){
+            var dir = target.transform.position - transform.position;
+            controller.lookDirection = new Vector2(dir.x * .1f,dir.z *.1f);       
+        }
+
     }
 
     List<GameObject> OrderByClosest(List<GameObject> objs, Vector3 origin){
