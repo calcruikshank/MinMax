633b408ac148a52dd5327ce9e9896f95a2db35f8
Onygox <93776257+lionelGB@users.noreply.github.com>
2022-07-16 15:26:33 -0400

    main menu skills roll

--- MinMax/Assets/Scripts/DieRoller.cs
+++ MinMax/Assets/Scripts/DieRoller.cs
@@ -11,13 +11,19 @@ public class DieRoller : MonoBehaviour
     public GameObject diePrefab;
     public Transform placeDiceHere, placePlayersHere;
     public DieScript currentDie;
-    public TMP_Text valueText;
+    public TMP_Text valueText, timerText;
     // public int playerIndex = 0;
-    public Button rollButton, resetButton;
+    public Button rollButton, resetButton, optionsButton, xButton, playButton;
     // public GameObject[] playerCursors;
     public MenuStatScript[] playerPanels;
     public List<PlayerCursor> pcs = new List<PlayerCursor>();
     public GameObject joinText;
+    public bool useBots = true, useSameStats = true;
+    public Toggle botsToggle, sameStatsToggle;
+    public List<string> sameStats = new List<string>();
+    public List<string> statStrings = new List<string>();
+    public Button[] addBotButtons;
+    public GameObject botPrefab;
 
     void Awake()
     {
@@ -31,9 +37,17 @@ public class DieRoller : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        Button_RollSameStats();
+        botsToggle.isOn = useBots;
+        sameStatsToggle.isOn = useSameStats;
+        Toggle_UseBots(true);
+    }
+
     public void Button_RollDie()
     {
-        // Debug.Log("rolling die");
+        Debug.Log("rolling die");
         if (currentDie != null && !EveryoneHasUsedCurrentDie()) return;
         rollButton.interactable = false;
         GameObject rolledDie = Instantiate(diePrefab, placeDiceHere.position, Quaternion.Euler(Random.Range(0, 180), Random.Range(0, 180), Random.Range(0, 180)));
@@ -48,6 +62,21 @@ public class DieRoller : MonoBehaviour
             // PlayerCursor pc = go.GetComponent<PlayerCursor>();
             pc.usedCurrentDie = false;
             pc.speed = 0;
+            pc.thisMSS.readyPanel.SetActive(false);
+        }
+        // StartCoroutine(Timer());
+        time = 6;
+    }
+
+    public void Button_RollSameStats()
+    {
+        List<string> statsCopy = new List<string>(statStrings);
+        for(int i = 0; i < 6; i++)
+        {
+            int randStat = Mathf.FloorToInt(Random.Range(0, statsCopy.Count));
+            string newStat = statsCopy[randStat];
+            sameStats.Add(newStat);
+            statsCopy.RemoveAt(randStat);
         }
     }
 
@@ -62,14 +91,24 @@ public class DieRoller : MonoBehaviour
         return true;
     }
 
+    public bool PlayersAreRollingStats()
+    {
+        foreach(PlayerCursor pc in pcs)
+        {
+            if (!pc.gameObject.activeSelf) continue;
+            if (pc.thisMSS.waitPanel.activeSelf) return true;
+        }
+        return false;
+    }
+
     public void CheckForDieUsage()
     {
         if (currentDie is null || !EveryoneHasUsedCurrentDie()) return;
         
         Destroy(currentDie.gameObject);
         currentDie = null;
-        valueText.text = "";
-        rollButton.interactable = true;
+        valueText.text = "-";
+        rollButton.interactable = !PlayersAreReady();
 
         // currentDie.thisMR.material = EveryoneHasUsedCurrentDie() ? usedDie : unusedDie;
     }
@@ -80,11 +119,12 @@ public class DieRoller : MonoBehaviour
         pi.transform.SetParent(placePlayersHere);
         PlayerCursor joinedPlayer = pi.GetComponent<PlayerCursor>();
         playerPanels[pcs.Count].gameObject.SetActive(true);
+        addBotButtons[pcs.Count].gameObject.SetActive(false);
         joinedPlayer.Initialize(playerPanels[pcs.Count]);
         pcs.Add(joinedPlayer);
         // playerIndex++;
         joinText.SetActive(pcs.Count < 4);
-        rollButton.interactable = pcs.Count > 0;
+        rollButton.interactable = !PlayersAreRollingStats();
         resetButton.interactable = pcs.Count > 0;
         ResetAllStats();
     }
@@ -97,8 +137,9 @@ public class DieRoller : MonoBehaviour
         pcs.Remove(leftPlayer);
         playerPanels[pcs.Count].gameObject.SetActive(false);
         joinText.SetActive(pcs.Count < 4);
-        rollButton.interactable = pcs.Count > 0;
+        rollButton.interactable = !PlayersAreRollingStats() && !PlayersAreReady();
         resetButton.interactable = pcs.Count > 0;
+        addBotButtons[pcs.Count].gameObject.SetActive(useBots);
     }
 
     public void SetPlayersMovement()
@@ -113,6 +154,7 @@ public class DieRoller : MonoBehaviour
     {
         foreach(MenuStatScript mss in playerPanels)
         {
+            if (!mss.gameObject.activeSelf) return;
             mss.ResetTexts();
         }
         
@@ -121,7 +163,7 @@ public class DieRoller : MonoBehaviour
             Destroy(currentDie.gameObject);
             currentDie = null;
         }
-        valueText.text = "";
+        valueText.text = "-";
         rollButton.interactable = true;
 
         foreach(PlayerCursor pc in pcs)
@@ -129,5 +171,113 @@ public class DieRoller : MonoBehaviour
             pc.speed = 200;
             pc.usedCurrentDie = false;
         }
+    }
+
+    public void RerollAllStats()
+    {
+        foreach(PlayerCursor pc in pcs)
+        {
+            StartCoroutine(pc.thisMSS.RollStats());
+        }
+    }
+    
+    public int time = 6;
+
+    public IEnumerator Timer()
+    {
+        timerText.text = ":0" + time.ToString();
+
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1);
+            time--;
+            timerText.text = ":0" + time.ToString();
+
+            foreach(PlayerCursor pc in pcs)
+            {
+                if (pc.thisMSS.isBot && !pc.usedCurrentDie)
+                {
+                    pc.ChooseRandomStat(currentDie.CurrentValue());
+                }
+            }
+        }
+
+        timerText.text = "";
+
+        foreach(PlayerCursor pc in pcs)
+        {
+            if (!pc.usedCurrentDie)
+            {
+                pc.ChooseRandomStat(currentDie.CurrentValue());
+            }
+            pc.usedCurrentDie = false;
+        }
+
+        if (currentDie != null)
+        {
+            Destroy(currentDie.gameObject);
+        }
+
+        currentDie = null;
+        valueText.text = "-";
+        rollButton.interactable = !PlayersAreReady();
+    }
+
+    public void Toggle_SameStats(bool s)
+    {
+        useSameStats = s;
+    }
+
+    public void Toggle_UseBots(bool b)
+    {
+        useBots = b;
+        // b = !PlayersAreReady();
+        // foreach(Button bu in addBotButtons)
+        for(int i = pcs.Count - 1; i >= 0; i--)
+        {
+            if (pcs[i].thisMSS.isBot)
+            {
+                PlayerCursor leftPlayer = pcs[i];
+                pcs.Remove(leftPlayer);
+                playerPanels[pcs.Count].gameObject.SetActive(false);
+                joinText.SetActive(pcs.Count < 4);
+                rollButton.interactable = !PlayersAreRollingStats() && !PlayersAreReady();
+                resetButton.interactable = pcs.Count > 0;
+                addBotButtons[pcs.Count].gameObject.SetActive(useBots);
+                Destroy(leftPlayer.gameObject);
+            }
+        }
+        for (int i = 0; i < addBotButtons.Length; i++)
+        {
+            // int buttonIndex = System.Array.IndexOf(AddBotButtons, bu);
+            bool isPlayer = i < pcs.Count;
+            addBotButtons[i].gameObject.SetActive(b && !isPlayer);
+        }
+    }
+
+    public void AddBot()
+    {
+        if (!useBots || currentDie != null) return;
+        GameObject botCursor = Instantiate(botPrefab);
+        botCursor.transform.SetParent(placePlayersHere);
+        PlayerCursor joinedPlayer = botCursor.GetComponent<PlayerCursor>();
+        playerPanels[pcs.Count].gameObject.SetActive(true);
+        addBotButtons[pcs.Count].gameObject.SetActive(false);
+        joinedPlayer.Initialize(playerPanels[pcs.Count], true);
+        pcs.Add(joinedPlayer);
+        joinText.SetActive(pcs.Count < 4);
+        rollButton.interactable = !PlayersAreRollingStats() && !PlayersAreReady();
+        resetButton.interactable = pcs.Count > 0;
+        ResetAllStats();
+    }
+
+    public bool PlayersAreReady()
+    {
+        foreach(PlayerCursor pc in pcs)
+        {
+            if (!pc.thisMSS.IsFull()) return false;
+        }
+        playButton.gameObject.SetActive(true);
+        return true;
     }
 }
--- MinMax/Assets/Scripts/PlayerCursor.cs
+++ MinMax/Assets/Scripts/PlayerCursor.cs
@@ -11,39 +11,97 @@ public class PlayerCursor : MonoBehaviour
     public Image playerImage;
     public RectTransform thisRT;
     public Vector2 moveVal;
-    public TMP_Text[] statTexts;
+    // public TMP_Text[] statTexts;
     public float speed = 100.0f;
     public bool usedCurrentDie = false;
     public MenuStatScript thisMSS;
 
-    public void Initialize(MenuStatScript newMenuStats)
+    public void Initialize(MenuStatScript newMenuStats, bool bot = false)
     {
         thisMSS = newMenuStats;
         playerLabel.text = newMenuStats.playerName;
         playerImage.color = newMenuStats.playerColor;
         playerLabel.color = newMenuStats.playerColor;
         newMenuStats.thisPC = this;
+        transform.position = newMenuStats.transform.position;
+        newMenuStats.ResetTexts();
+        // newMenuStats.isBot = bot;
+        newMenuStats.botToggle.isOn = bot;
+        StartCoroutine(newMenuStats.RollStats());
     }
 
     public void OnMove(InputValue value)
     {
+        if (thisMSS.waitPanel.activeSelf || thisMSS.isBot) return;
         moveVal = value.Get<Vector2>();
     }
 
     public void OnFire()
     {
         Debug.Log(gameObject.name + " fire");
-        if (Vector3.Distance(transform.position, DieRoller.singleton.rollButton.transform.position) < 90)
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.rollButton.transform.position) < 90 && DieRoller.singleton.rollButton.interactable && !DieRoller.singleton.xButton.gameObject.activeSelf)
         {
             DieRoller.singleton.rollButton.onClick.Invoke();
             return;
         }
-        if (Vector3.Distance(transform.position, DieRoller.singleton.resetButton.transform.position) < 45)
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.resetButton.transform.position) < 45 && DieRoller.singleton.resetButton.interactable && DieRoller.singleton.xButton.gameObject.activeSelf)
         {
             DieRoller.singleton.resetButton.onClick.Invoke();
             return;
         }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.optionsButton.transform.position) < 65 && DieRoller.singleton.optionsButton.interactable && !DieRoller.singleton.xButton.gameObject.activeSelf)
+        {
+            DieRoller.singleton.optionsButton.onClick.Invoke();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.xButton.transform.position) < 60 && DieRoller.singleton.xButton.interactable && DieRoller.singleton.xButton.gameObject.activeSelf)
+        {
+            DieRoller.singleton.xButton.onClick.Invoke();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.botsToggle.transform.position) < 60 && DieRoller.singleton.botsToggle.interactable && DieRoller.singleton.xButton.gameObject.activeSelf)
+        {
+            DieRoller.singleton.botsToggle.isOn = !DieRoller.singleton.botsToggle.isOn;
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.sameStatsToggle.transform.position) < 60 && DieRoller.singleton.sameStatsToggle.interactable && DieRoller.singleton.xButton.gameObject.activeSelf)
+        {
+            DieRoller.singleton.sameStatsToggle.isOn = !DieRoller.singleton.sameStatsToggle.isOn;
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.addBotButtons[0].transform.position) < 60 && DieRoller.singleton.addBotButtons[0].interactable && DieRoller.singleton.addBotButtons[0].gameObject.activeSelf)
+        {
+            DieRoller.singleton.addBotButtons[0].onClick.Invoke();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.addBotButtons[1].transform.position) < 60 && DieRoller.singleton.addBotButtons[1].interactable && DieRoller.singleton.addBotButtons[1].gameObject.activeSelf)
+        {
+            DieRoller.singleton.addBotButtons[1].onClick.Invoke();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.addBotButtons[2].transform.position) < 60 && DieRoller.singleton.addBotButtons[2].interactable && DieRoller.singleton.addBotButtons[2].gameObject.activeSelf)
+        {
+            DieRoller.singleton.addBotButtons[2].onClick.Invoke();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, DieRoller.singleton.addBotButtons[3].transform.position) < 60 && DieRoller.singleton.addBotButtons[3].interactable && DieRoller.singleton.addBotButtons[3].gameObject.activeSelf)
+        {
+            DieRoller.singleton.addBotButtons[3].onClick.Invoke();
+            return;
+        }
+
         if (usedCurrentDie || DieRoller.singleton.currentDie is null || string.IsNullOrEmpty(DieRoller.singleton.valueText.text)) return;
+
         float closestDistance = 10.0f;
         TMP_Text closestText = null;
         foreach(TMP_Text tmp in thisMSS.statTexts)
@@ -62,6 +120,7 @@ public class PlayerCursor : MonoBehaviour
             closestText.text = DieRoller.singleton.valueText.text;
             usedCurrentDie = true;
             DieRoller.singleton.CheckForDieUsage();
+            thisMSS.readyPanel.SetActive(true);
         }
     }
 
@@ -69,5 +128,29 @@ public class PlayerCursor : MonoBehaviour
     {
         thisRT.anchoredPosition += moveVal * speed * Time.deltaTime;
         thisRT.anchoredPosition = new Vector2(Mathf.Clamp(thisRT.anchoredPosition.x, (-Screen.width/2) + 40, (Screen.width/2) - 40), Mathf.Clamp(thisRT.anchoredPosition.y, (-Screen.height/2)+20, (Screen.height/2)-20));
+    }
+
+    void OnDisable()
+    {
+        thisMSS.isBot = true;
+    }
+
+    public void ChooseRandomStat(int rand)
+    {
+        List<TMP_Text> unusedStatTexts = new List<TMP_Text>();
+        foreach(TMP_Text tt in thisMSS.statTexts)
+        {
+            if (string.IsNullOrEmpty(tt.text))
+            {
+                unusedStatTexts.Add(tt);
+            }
+        }
+        Debug.Log(unusedStatTexts.Count);
+        int randomText = Mathf.FloorToInt(Random.Range(0, unusedStatTexts.Count));
+        Debug.Log(randomText);
+        unusedStatTexts[randomText].text = rand.ToString();
+        usedCurrentDie = true;
+        DieRoller.singleton.CheckForDieUsage();
+        thisMSS.readyPanel.SetActive(true);
     }
 }
