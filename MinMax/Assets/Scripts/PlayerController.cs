b564561ec30909feef6941c9226162f9540b2726
Onygox <93776257+lionelGB@users.noreply.github.com>
2022-07-17 04:48:32 -0400

    mana slidere ui

--- MinMax/Assets/Scripts/PlayerController.cs
+++ MinMax/Assets/Scripts/PlayerController.cs
@@ -251,7 +251,8 @@ public class PlayerController : MonoBehaviour
     
     private void TrackAnimation(AnimatorStateInfo stateInfo)
     {
-        currentMana += Time.deltaTime * stats.manaRegenRate;
+        currentMana = Mathf.Clamp(currentMana + (Time.deltaTime * stats.manaRegenRate), 0, stats.manaPool);
+        thisHPS.manaSlider.value = GameManager.g.Remap(currentMana, 0, stats.manaPool, 0, 1);
         if (currentMana < stats.manaCost) return;
         if (fireDownPressed && (currentMana > stats.manaCost))
         {
