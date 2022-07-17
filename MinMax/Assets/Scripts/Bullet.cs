add8ecf7843173e92dc9458acaee92bba593b1e7
calcruikshank <calcruikshank@gmail.com>
2022-07-16 20:23:01 -0600

    l;ight

--- MinMax/Assets/Scripts/Bullet.cs
+++ MinMax/Assets/Scripts/Bullet.cs
@@ -52,6 +52,7 @@ public class Bullet : MonoBehaviour
     {
         if (other.GetComponent<PlayerController>() != null)
         {
+            Debug.Log("Bollenan");
             var objectHit = other.GetComponent<PlayerController>();
             if(objectHit != playerOwningBullet){
                 objectHit.TakeDamage(attackDamage);
